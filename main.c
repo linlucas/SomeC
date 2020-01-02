bfc3280ab45e045dc8518995da37a42e88335b2d
Lucas Lin <linlucas33@gmail.com>
2020-01-01 23:10:47 -0800

    Made enums

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
-int main() {
+enum color {
+    red, green, blue, yellow
+};
 
+int main() {
+    enum color screenColor = red;
+    printf("%d is the number", screenColor);
 }
