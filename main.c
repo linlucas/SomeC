aadb5af4fd361826aa0892fe7f90b53075ca0071
Lucas Lin <linlucas33@gmail.com>
2020-01-09 00:32:41 -0800

    Added malloc

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "projectHeaders.h"
+#include <string.h>
+
+char *duplicate(const char *);
 
 int main(void) {
-    
+    char *something = "hi";
+    char *hello = duplicate(something);
+    printf("%s", hello);
+}
+
+char *duplicate(const char *pString) {
+    char *pNew = malloc(sizeof(*pString) + 1);
+    if (!pString) {
+        return NULL;
+    }
+    strcpy(pNew, pString);
+    free(pNew);
+    return pNew;
 }
